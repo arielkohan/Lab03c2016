1fb59a368679881a7f1c390abb6eeec6a1af678c
Ariel Kohan <arielkohan94@gmail.com>
2016-09-27 15:37:17 -0300

    Add menu item and create new empty activity.

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/lab03c2016/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/lab03c2016/MainActivity.java
@@ -3,8 +3,8 @@ package ar.edu.utn.frsf.isi.dam.lab03c2016;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -26,9 +26,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       /* toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.title);
-        setSupportActionBar(toolbar);*/
         listTrabajos = new ArrayList<Trabajo>();
         listTrabajos.addAll(Arrays.asList(Trabajo.TRABAJOS_MOCK));
 
@@ -39,6 +36,25 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuCrearOferta: {
+                Toast.makeText(MainActivity.this, "Crear oferta clickeado", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            default:{
+              return super.onOptionsItemSelected(item);
+            }
+        }
     }
 
 
